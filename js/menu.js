23850690925bc1875106723a2bb771e6bc6be7dd
HotelCopantl <99454505+HotelCopantl@users.noreply.github.com>
2022-02-11 18:52:14 -0600

    Delete js directory

--- js/menu.js
+++
@@ -1,6 +0,0 @@
-let btnmenu = document.getElementById('btnmenu');
-let menu = document.getElementById('menu');
-btnmenu.addEventListener('click', function () {
-    'use strict';
-    menu.classList.toggle('mostrar');
-});
