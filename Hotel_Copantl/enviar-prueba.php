115d49ce0cea71ae0b04bac573cb0bc7f9f2d138
HOTEL COPANTL <99454505+HotelCopantl@users.noreply.github.com>
2022-02-13 16:10:58 -0600

    Add files via upload

---
+++ Hotel_Copantl/enviar-prueba.php
@@ -0,0 +1,58 @@
+<?php
+$nombre = $_POST["nombre"];
+$correo = $_POST["correo"];
+$telefono = $_POST["telefono"];
+$mensaje = $_POST["mensaje"];
+
+$body = "Nombre: " . $nombre . "<br>Correo: " . $correo . "<br>Telefono: " . $telefono . "<br>Mensaje: " . $mensaje;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+ 
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                                       //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'ccarnavalero@gmail.com';                     //SMTP username
+    $mail->Password   = 'Emendoza2020$';                               //SMTP password
+    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+
+    //Recipients
+    $mail->setFrom('kenneth@copantl.com', $nombre);
+    $mail->addAddress('kennethchavez794@gmail.com');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Consultas desde Pagina WEB';
+    $mail->Body    = $body;
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients   11-02-2021';
+    $mail->CharSet = 'UTF-8';
+    $mail->send();
+     
+    echo '<script>
+        alert("El mensaje se envió correctamente");
+         window.history.go(-1);
+         </script>';
+    
+    
+     header('Location:index.html');
+       
+        
+} catch (Exception $e) {
+    echo 'Message could not be sent // No se pudo enviar el mensaje. Mailer Error: ', $mail->ErrorInfo;
+}
+
+
+    
+        
